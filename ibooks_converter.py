de80c6a882bb3081821b3c2c7cd26c68d336c833
liam-herbst <lherbst.hba2021@ivey.ca>
2020-10-11 21:07:05 -0400

    Working on file formatting

--- ibooks_converter.py
+++ ibooks_converter.py
@@ -1,17 +1,29 @@
 #This is a script for converting the ugly iBooks notes exports into a clean Markdown file
 
-import ex_notes.txt
+#import ~/home/liamherbst/github/projects/ibook_highlighter_converter/ex_notes.txt
+import datetime
+#from os import path
 
-with open('ex_notes.txt') as notes:
-    lines = notes.readlines()
+#test = open(os.path.join(home/liamherbst/github/projects/ibook_highlighter_converter/ex_notes.txt, 'ex_notes.txt'))
+
+#print(test)
+
+with open('ex_notes.txt, 'r') as old_notes:
+    lines = old_notes.readlines()
 
     def convert_lines(lines):
-        if line in text == #(insert date and time format (see codecademy cheatsheets)):
-            delete line
-        elif line in text == Chaper + number format:
-            add line (unless it already exists) & remove the page #
-        #maybe not neccessary elif line == '\n':
-            print('\n')
-        else:
-            add line (+add page number from the line above if possible)
-        #see if I need to do anything to account
+        for line in lines:
+            if (line.startswith('August 3')):
+            #datetime.date(%B, " " + %d, ", " + %Y)
+                pass
+            #elif line in text == Chaper + number format:
+                #add line (unless it already exists) & remove the page #
+            #maybe not neccessary elif line == '\n':
+                #print('\n')
+            else:
+                new_notes.write(line) 
+                #(+add page number from the line above if possible)
+            #see if I need to do anything to account
+
+    with open('new_notes.md', 'w') as new_notes:
+        print('test', file=new_notes)
