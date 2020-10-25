f057e4a52b4ebc954eadf1a0584dd17df22fef68
Liam Herbst <liamherbst29@gmail.com>
2020-10-25 18:26:32 -0400

    Successfully removed dates and finalized line formatting

--- ibooks_converter.py
+++ ibooks_converter.py
@@ -1,61 +1,84 @@
 #See if it's possible to modify the file name to reflect the book
 ## E.g. Formatted: Almanack of Naval
+#Ideally I'll be able to automatically make a bulleted list for the notes under each chapter 
+#for the time being I could just add a "- " for each item so I can input that way into notion
 
+### Import modules and files ###
 from datetime import datetime
 import sys
 import os
 
-# Linked in the old notes files to this script
-ibook_highlighter_converter = os.path.dirname(os.path.abspath(__file__))
-old_notes = open(os.path.join(ibook_highlighter_converter, 'ex_notes.txt'))
-old_lines = old_notes.readlines()
+original_notes = open('ex_notes.txt', 'r')
+original_lines = original_notes.readlines()
+
+# Confirm highlights properly converted to a list
+#print(type(original_lines))
+
+# Confirm highlights properly outputted
+#print(original_lines)
+
+### Format Import ### 
+def remove_spaces(original_lines):
+    removed_space_lines = []
+    for line in original_lines:
+        if line.startswith('\n'):
+            pass
+        elif line.endswith('\n'):
+            remove_line_space = line[:-1]
+            removed_space_lines.append(remove_line_space)
+        else:
+            removed_space_lines.append(line)
+    return removed_space_lines
+
+# Save output
+removed_spaces = remove_spaces(original_lines)
+
+# Test
+#print(removed_spaces)
+
+### Eliminate date stamps ###
+date_format = "%B %d, %Y"
+month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+#day = list(range(1, 32))
+
+def eliminate_dates(removed_spaces):
+    removed_date_lines = []
+    for line in removed_spaces:
+        # This is not ideal since it removes any line that starts with a month
+        # See if I can program this later to make it specific for each day
+        if any (line.startswith(m) for m in month):
+            pass
+        else:
+            removed_date_lines.append(line)
+    return removed_date_lines
+
+# Save output
+eliminated_dates = eliminate_dates(removed_spaces)
+
+# Test
+print(eliminated_dates)
+
+### Step 2: Formatting Chapter Titles ###
+#def format_page_numbers(eliminate_dates):
 
 # Delete the page number (denoted by ", p. ") from the end of the chapter title line and appends it to the end of the next line
 #a Not all chapter titles have this so I'll need to account for that (it shoudl be accounted for anyway)
 ## So chapter titles become duplicates and notes have accurate page numbers 
-
-# Identify which lines are duplicates and switch them to h2
-
-# Delete duplicate lines but leave the first instance of each duplicate
-
-date_format = "%B %d, %Y"  
-
-def convert_lines(lines):
-        sys.stdout = fn
-        for line in lines:
-            # Identifies duplicate lines (chapters) and changes them to headers (h2)
-            counter = 0 
-            if line[counter] == line[counter + 1]:
-                fn.write("<h2>" + line + "</h2>")
-                counter += 1
-
-            ## Removes duplicate lines 
+## Removes duplicate lines 
+            # https://www.geeksforgeeks.org/python-list-remove/
             # https://www.geeksforgeeks.org/python-ways-to-remove-duplicates-from-list/
             # https://careerkarma.com/blog/python-remove-duplicates-from-list/#:~:text=There%20are%20a%20couple%20of,you%20have%20into%20a%20set.
+    # Identify which lines are duplicates and switch them to h2
+    #fn.write("<h2>" + line + "</h2>")
+# Delete duplicate lines but leave the first instance of each duplicate
 
-            # Checks to see if the line is a date and subsequently eliminates dates
-            try:
-                datetime.strptime(line, date_format)
-                pass
-            except ValueError:
-                pass
-            # Checks to see if the line is a duplicate (chapter titles are duplicates) and subsequently eliminates duplicates
-            #if line in text == Chaper + number format:
-                #add line (unless it already exists) & remove the page #
-            #maybe not neccessary elif line == '\n':
-                #print('\n')
-            # Returns highlights and notes
-            else:
-                fn.write("<h2>" + line + "</h2>")
-                #(+add page number from the line above if possible)
-            #see if I need to do anything to account
+
+
         #Add line that drives people back to my website/twitter/youtube (my CTA)
-        return fn
+
+
+### Step x: Export to HTML file ###
 
 with open('formatted_notes.html', 'w') as fn:
-    print("ending equation name [literally]", file=fn)
-
-    print(convert_lines(old_lines))
-
-with open('new_notes.html', 'w') as new_notes:
-    print(holy, file=new_notes)
+    for line in eliminated_dates:
+        print(line, file=fn)
---
+++ testing_zone.py
@@ -0,0 +1,26 @@
+import sys
+
+#with open("ex_notes.txt", "r") as en:
+ #   print(ex_notes)
+
+
+
+ ### Link the old notes file to this script ###
+#ibook_highlighter_converter = os.path.dirname(os.path.abspath(__file__))
+#old_notes = open(os.path.join(ibook_highlighter_converter, 'ex_notes.txt'))
+
+string = "aositn aoitnea \n"
+
+def test():
+    if string[:-2] == '\n':
+        return 'hell ya'
+print(test())
+
+
+# Identify dates as strings
+        #if line.format(datetime.strftime(line, date_format)):
+        #    eliminate_dates.append(line)
+#print(eliminate_dates(old_lines))
+# no_date_lines = [line for line in old_lines if line == datetime.strptime(line, date_format)]
+#newer_lines = old_lines.remove(line == date_format)
+#print(newer_lines)
